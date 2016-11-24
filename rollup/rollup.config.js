b199d3e09871993f04f92038fc62ee6cc96c6a8f
Tsutomu Kawamura <cognitom@gmail.com>
2016-11-24 19:59:25 +0900

    Fix rollup example

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,19 +1,15 @@
 import riot from 'rollup-plugin-riot'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
+import buble from 'rollup-plugin-buble'
 
 export default {
   entry: 'src/main.js',
   dest: 'dist/bundle.js',
   plugins: [
     riot(),
-    nodeResolve({
-      jsnext: true, // if provided in ES6
-      main: true, // if provided in CommonJS
-      browser: true // if provided for browsers
-    }),
+    nodeResolve({ jsnext: true }),
     commonjs(),
-    babel()
+    buble()
   ]
 }

2416d2f3dd2277de932c88c1e8fa1607d92874cb
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-03-08 22:02:28 +0100

    updated: webpack 4

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -10,20 +10,22 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.tag$/,
         exclude: /node_modules/,
-        loader: 'riot-tag-loader',
-        query: {
-          type: 'es6', // transpile the riot tags using babel
-          hot: true
-        }
+        use: [{
+          loader: 'riot-tag-loader',
+          query: {
+            type: 'es6', // transpile the riot tags using babel
+            hot: true
+          }
+        }]
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        use: ['babel-loader']
       }
     ]
   }

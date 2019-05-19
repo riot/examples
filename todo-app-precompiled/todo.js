b09f23c33a5a0e9a322eb0db66ce51fcb2e22d6c
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-05-19 21:48:32 +0200

    added: precompiled example

--- todo-app-precompiled/todo.js
+++ todo-app-precompiled/todo.js
@@ -1,36 +1,160 @@
+var todo = {
+  'css': null,
 
-riot.tag2('todo', '<h3>{opts.title}</h3> <ul> <li each="{items.filter(whatShow)}"> <label class="{completed: done}"> <input type="checkbox" checked="{done}" onclick="{parent.toggle}"> {title} </label> </li> </ul> <form onsubmit="{add}"> <input ref="input" onkeyup="{edit}"> <button disabled="{!text}">Add #{items.filter(whatShow).length + 1}</button> <button type="button" disabled="{items.filter(onlyDone).length == 0}" onclick="{removeAllDone}"> X{items.filter(onlyDone).length} </button> </form>', '', '', function(opts) {
-    this.items = opts.items
+  'exports': {
+    onBeforeMount(props, state) {
+      // initial state
+      this.state = {
+        items: props.items,
+        text: ''
+      };
+    },
 
-    this.edit = function(e) {
-      this.text = e.target.value
-    }.bind(this)
+    edit(e) {
+      // update only the text state
+      this.update({
+        text: e.target.value
+      });
+    },
 
-    this.add = function(e) {
-      if (this.text) {
-        this.items.push({ title: this.text })
-        this.text = this.refs.input.value = ''
+    add(e) {
+      e.preventDefault();
+
+      if (this.state.text) {
+        this.update({
+          items: [
+            ...this.state.items,
+            // add a new item
+            {title: this.state.text}
+          ],
+          text: ''
+        });
       }
-      e.preventDefault()
-    }.bind(this)
+    },
 
-    this.removeAllDone = function(e) {
-      this.items = this.items.filter(function(item) {
-        return !item.done
-      })
-    }.bind(this)
+    toggle(item) {
+      item.done = !item.done;
+      // trigger a component update
+      this.update();
+    }
+  },
 
-    this.whatShow = function(item) {
-      return !item.hidden
-    }.bind(this)
+  'template': function(template, expressionTypes, bindingTypes, getComponent) {
+    return template(
+      '<h3 expr0><!----></h3><ul><li expr1></li></ul><form expr4><input expr5/><button expr6><!----></button></form>',
+      [{
+        'redundantAttribute': 'expr0',
+        'selector': '[expr0]',
 
-    this.onlyDone = function(item) {
-      return item.done
-    }.bind(this)
+        'expressions': [{
+          'type': expressionTypes.TEXT,
+          'childNodeIndex': 0,
 
-    this.toggle = function(e) {
-      var item = e.item
-      item.done = !item.done
-      return true
-    }.bind(this)
-});
+          'evaluate': function(scope) {
+            return scope.props.title;
+          }
+        }]
+      }, {
+        'type': bindingTypes.EACH,
+        'getKey': null,
+        'condition': null,
+
+        'template': template('<label expr2><input expr3 type="checkbox"/><!----></label>', [{
+          'redundantAttribute': 'expr2',
+          'selector': '[expr2]',
+
+          'expressions': [{
+            'type': expressionTypes.TEXT,
+            'childNodeIndex': 1,
+
+            'evaluate': function(scope) {
+              return ['\n        ', scope.item.title, '\n      '].join('');
+            }
+          }, {
+            'type': expressionTypes.ATTRIBUTE,
+            'name': 'class',
+
+            'evaluate': function(scope) {
+              return scope.item.done ? 'completed' : null;
+            }
+          }]
+        }, {
+          'redundantAttribute': 'expr3',
+          'selector': '[expr3]',
+
+          'expressions': [{
+            'type': expressionTypes.ATTRIBUTE,
+            'name': 'checked',
+
+            'evaluate': function(scope) {
+              return scope.item.done;
+            }
+          }, {
+            'type': expressionTypes.EVENT,
+            'name': 'onclick',
+
+            'evaluate': function(scope) {
+              return () => scope.toggle(scope.item);
+            }
+          }]
+        }]),
+
+        'redundantAttribute': 'expr1',
+        'selector': '[expr1]',
+        'itemName': 'item',
+        'indexName': null,
+
+        'evaluate': function(scope) {
+          return scope.state.items;
+        }
+      }, {
+        'redundantAttribute': 'expr4',
+        'selector': '[expr4]',
+
+        'expressions': [{
+          'type': expressionTypes.EVENT,
+          'name': 'onsubmit',
+
+          'evaluate': function(scope) {
+            return scope.add;
+          }
+        }]
+      }, {
+        'redundantAttribute': 'expr5',
+        'selector': '[expr5]',
+
+        'expressions': [{
+          'type': expressionTypes.EVENT,
+          'name': 'onkeyup',
+
+          'evaluate': function(scope) {
+            return scope.edit;
+          }
+        }]
+      }, {
+        'redundantAttribute': 'expr6',
+        'selector': '[expr6]',
+
+        'expressions': [{
+          'type': expressionTypes.TEXT,
+          'childNodeIndex': 0,
+
+          'evaluate': function(scope) {
+            return ['\n      Add #', scope.state.items.length + 1, '\n    '].join('');
+          }
+        }, {
+          'type': expressionTypes.ATTRIBUTE,
+          'name': 'disabled',
+
+          'evaluate': function(scope) {
+            return !scope.state.text;
+          }
+        }]
+      }]
+    );
+  },
+
+  'name': 'todo'
+};
+
+export default todo;
---
+++ todo-app-precompiled/todo.riot
@@ -0,0 +1,59 @@
+<todo>
+  <h3>{ props.title }</h3>
+
+  <ul>
+    <li each={ item in state.items }>
+      <label class={ item.done ? 'completed' : null }>
+        <input
+          type="checkbox"
+          checked={ item.done }
+          onclick={ () => toggle(item) } />
+        { item.title }
+      </label>
+    </li>
+  </ul>
+
+  <form onsubmit={ add }>
+    <input onkeyup={ edit } />
+    <button disabled={ !state.text }>
+      Add #{ state.items.length + 1 }
+    </button>
+  </form>
+
+  <script>
+    export default {
+      onBeforeMount(props, state) {
+        // initial state
+        this.state = {
+          items: props.items,
+          text: ''
+        }
+      },
+      edit(e) {
+        // update only the text state
+        this.update({
+          text: e.target.value
+        })
+      },
+      add(e) {
+        e.preventDefault()
+
+        if (this.state.text) {
+          this.update({
+            items: [
+              ...this.state.items,
+              // add a new item
+              {title: this.state.text}
+            ],
+            text: ''
+          })
+        }
+      },
+      toggle(item) {
+        item.done = !item.done
+        // trigger a component update
+        this.update()
+      }
+    }
+  </script>
+</todo>

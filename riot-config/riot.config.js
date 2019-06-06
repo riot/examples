43f2433551ef6fe84e4123f972892de960684550
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-06-06 23:58:56 +0200

    updated: riot config example

--- riot-config/riot.config.js
+++ riot-config/riot.config.js
@@ -1,19 +1,5 @@
-import buble from 'buble'
-import postcss from 'postcss'
-import cssnext from 'postcss-cssnext'
-
 export default {
-  type: 'buble',
-  style: 'cssnext',
-  parsers: {
-    js: { buble: js => buble.transform(js) },
-    css: { cssnext: (tagName, css) => {
-      // A small hack: it passes :scope as :root to PostCSS.
-      // This make it easy to use css variables inside tags.
-      css = css.replace(/:scope/g, ':root')
-      css = postcss([cssnext]).process(css).css
-      css = css.replace(/:root/g, ':scope')
-      return css
-    }}
-  }
+  sourcemap: 'inline',
+  output: 'dist/main.js',
+  format: 'umd'
 }
---
+++ riot-config/src/main.js
@@ -0,0 +1,4 @@
+import {component} from 'riot'
+import App from './app.riot'
+
+component(App)(document.getElementById('root'))

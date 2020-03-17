72ef433c782fea1e7be2ff7379dae3c6d6736fa5
kkeeth <zensin0082@gmail.com>
2020-03-17 16:59:26 +0900

    updated directory structure of parcel example

--- parcel/src/main.js
+++ parcel/src/main.js
@@ -1,6 +1,6 @@
 import '@riotjs/hot-reload'
 import {component} from 'riot'
-import Random from './random.riot'
+import Random from './components/random.riot'
 
 component(Random)(document.getElementById('app'), {
   title: 'Hi there!'
